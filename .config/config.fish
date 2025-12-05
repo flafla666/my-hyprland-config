bd1b079315331e8b36146cb77786df738c5b2d0b
Flareon <flareon1006@proton.me>
2025-12-05 21:12:25 +0100

    Add files via upload

---
+++ .config/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+alias maj='paru && flatpak update'
+alias nvgpu='env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only'
+end
+
+fastfetch
